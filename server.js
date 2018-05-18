072ca8becd0f264235484b44b0f1ed54c123a4ab
Patrick Ford <patrickford@msn.com>
2018-05-17 23:10:40 -0700

    simple server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Server is now running on port 3000');
+});
